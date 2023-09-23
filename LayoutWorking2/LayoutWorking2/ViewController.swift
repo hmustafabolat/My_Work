c1cd62b53b79d88f0eb8720160648fcef1648eca
hmb <mstfhll353@gmail.com>
2023-09-23 11:48:52 +0300

    Last app added

---
+++ LayoutWorking2/LayoutWorking2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutWorking2
+//
+//  Created by Musti on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
